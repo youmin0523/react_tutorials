09eb162fc31b9c5a2936d648c59624c0cef2fa03
youmin0523 <youminsu0523@gmail.com>
2026-01-20 16:40:41 +0900

    react_hook

---
+++ react_hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline"> React Hooks </h2>
+    </>
+  )
+}
+
+export default App
